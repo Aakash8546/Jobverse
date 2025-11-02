6d4f25b8603f4d7ae66d0260010a5fb618c9df20
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2025-11-03 01:19:00 +0530

    setting variables for api response

---
+++ src/main/java/com/example/jobverse/dto/auth/ApiResponse.java
@@ -0,0 +1,24 @@
+package com.example.jobverse.dto.auth;
+
+import com.example.jobverse.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private Boolean success;
+    private String message;
+    private Object data;
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+}
