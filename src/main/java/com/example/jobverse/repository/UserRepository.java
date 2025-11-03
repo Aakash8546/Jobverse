9d84bab99300043590bd9b90d029d2ba8bf87eb7
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2025-11-03 14:53:28 +0530

    implementing user repo

---
+++ src/main/java/com/example/jobverse/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.jobverse.repository;
+
+import com.example.jobverse.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+    List<User> findByRole(User.UserRole role);
+}
