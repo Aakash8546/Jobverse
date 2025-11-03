65790a1cd192877c3e2375bafb15f43df8c7974c
Aakash Srivastava <aakashsrivastava@Aakashs-Macbook.local>
2025-11-03 15:13:45 +0530

    repo layer for job seeker profile

---
+++ src/main/java/com/example/jobverse/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,22 @@
+package com.example.jobverse.repository;
+
+import com.example.jobverse.entity.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Long>
+{
+    Optional<JobSeekerProfile> findByUserId(Long userId);
+
+
+    Page<JobSeekerProfile> searchProfiles(@Param("keyword") String keyword, Pageable pageable);
+}
